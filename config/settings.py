314916da22440576d8d9a69b86ce3cde8e12fd8a
goldendevuz <goldendevuz@gmail.com>
2025-03-01 15:35:59 +0500

    15.35.01.03.2025

--- config/settings.py
+++ config/settings.py
@@ -140,6 +140,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'SEARCH_PARAM': 'term',
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 10,
 }
